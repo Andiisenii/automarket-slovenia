ac9ea855f7ef0f6c0e4e0a58069a814658977921
Andi Iseni <ai30978@seeu.edu.mk>
2026-04-29 16:45:44 +0200

    chore: remove temp files

--- check_encoding.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-with open(r'C:\Users\andii\OneDrive\Desktop\car-marketplace\src\lib\data.js', 'rb') as f:
-    data = f.read()
-
-# Find avtodom section
-idx = data.find(b"'avtodom':")
-if idx >= 0:
-    print("avtodom section:")
-    print(data[idx:idx+400])
